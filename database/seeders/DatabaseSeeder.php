c03f38db5c5a463adfe318b04cfd32e32fbfce99
Dami <damilarelamine@gmail.com>
2025-02-20 16:07:11 +0100

    chore: added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,50 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    use Illuminate\Database\Seeder;
+    use App\Models\Product;
+    use App\Models\Category;
+    use App\Models\Account;
+    use Faker\Factory as Faker;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    class DatabaseSeeder extends Seeder
     {
-        // \App\Models\Account::factory(10)->create();
+        public function run(): void
+        {
+            $faker = Faker::create();
 
-        // \App\Models\Account::factory()->create([
-        //     'name' => 'Test Account',
-        //     'email' => 'test@example.com',
-        // ]);
+            if (Category::count() == 0) {
+                Category::insert([
+                    ['name' => 'Electronics'],
+                    ['name' => 'Fashion'],
+                    ['name' => 'Home Appliances'],
+                    ['name' => 'Books'],
+                    ['name' => 'Sports'],
+                ]);
+            }
+
+
+            if (Account::count() == 0) {
+                Account::insert([
+                    ['name' => 'Demo Account 1'],
+                    ['name' => 'Demo Account 2'],
+                    ['name' => 'Demo Account 3'],
+                ]);
+            }
+
+            $categories = Category::pluck('id')->toArray();
+            $accounts = Account::pluck('id')->toArray();
+
+            foreach (range(1, 50) as $index) {
+                Product::create([
+                    'name' => $faker->word(),
+                    'description' => $faker->sentence(),
+                    'price' => $faker->randomFloat(2, 10, 500),
+                    'stock_quantity' => $faker->numberBetween(1, 100),
+                    'category_id' => $faker->randomElement($categories),
+                    'account_id' => $faker->randomElement($accounts),
+                ]);
+            }
+        }
     }
-}
